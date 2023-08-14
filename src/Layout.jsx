cdbe1c543eaca843421cb470c043bcd6b742cbe6
Divyeshkalariya <divyeshkalariya26@gmail.com>
2023-08-14 09:32:52 +0530

    update data

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -33,7 +33,7 @@ export default function Layout() {
         }
     };
     const scrollToTop = () => {
-        window.scrollTo(0,0)
+        window.scrollTo(0, 0)
     };
     window.addEventListener('scroll', toggleVisible);
 
@@ -237,8 +237,15 @@ export default function Layout() {
                                 </span>
                             </Link>
                         </Nav>
-                        <Link to='/cart' className='text-decoration-none'>
-                            <Button variant='warning' className='rounded-0 border-0 d-flex justify-content-evenly align-items-center cart' >
+                        <Link
+                            to='/cart'
+                            spy="true"
+                            smooth={true}
+                            offset={-40}
+                            duration={500}
+                            className='text-decoration-none'
+                        >
+                            <Button variant='warning' className='rounded-0 border-0 d-flex justify-content-evenly align-items-center cart' data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show" onClick={Hidemenu}>
                                 <svg width="14" className='me-1' height="14" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
                                     <path className="_1bS9ic" d="M15.32 2.405H4.887C3 2.405 2.46.805 2.46.805L2.257.21C2.208.085 2.083 0 1.946 0H.336C.1 0-.064.24.024.46l.644 1.945L3.11 9.767c.047.137.175.23.32.23h8.418l-.493 1.958H3.768l.002.003c-.017 0-.033-.003-.05-.003-1.06 0-1.92.86-1.92 1.92s.86 1.92 1.92 1.92c.99 0 1.805-.75 1.91-1.712l5.55.076c.12.922.91 1.636 1.867 1.636 1.04 0 1.885-.844 1.885-1.885 0-.866-.584-1.593-1.38-1.814l2.423-8.832c.12-.433-.206-.86-.655-.86" fill="#fff"></path>
                                 </svg>
